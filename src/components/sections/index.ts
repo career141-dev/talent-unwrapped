e289e6adc5639972511d9b55d96545f4f3eede7a
sanjeev200009 <sanjaysanjeev2000@gmail.com>
2026-02-04 19:15:59 +0530

    last verion

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -3,4 +3,3 @@ export { SubmitFormSection } from "./SubmitFormSection";
 export { FooterSection } from "./FooterSection";
 export { ContactUsSection } from "./ContactUsSection";
 export { MobileCarouselSection } from "./MobileCarouselSection";
-
